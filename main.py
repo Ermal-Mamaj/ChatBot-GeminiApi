a913178f7dd0aad96b78db7393ad97c4a1fcb65e
Ermal Mamaj <ermalmamaj@gmail.com>
2025-03-27 10:59:17 +0100

    changes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import google.generativeai as genai
 
+<<<<<<< HEAD
 # Configure the AI model
 genai.configure(api_key="AIzaSyCW1qOBRwNIHMdCjHzSsZ0O_lHKiJUSfUw")
 model = genai.GenerativeModel("gemini-1.5-flash")
@@ -19,17 +20,35 @@ class ChatInput(BaseModel):
     message: str
 
 # Route for the home page
+=======
+genai.configure(api_key="AIzaSyCW1qOBRwNIHMdCjHzSsZ0O_lHKiJUSfUw")
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+class ChatInput(BaseModel):
+    message: str
+
+>>>>>>> 694168a (render deployment workflow)
 @app.get("/", response_class=HTMLResponse)
 async def get_index():
     with open("static/index.html", "r") as file:
         return file.read()
 
+<<<<<<< HEAD
 # Chat API endpoint
+=======
+>>>>>>> 694168a (render deployment workflow)
 @app.post("/chat")
 async def chat_endpoint(chat_input: ChatInput):
     user_message = chat_input.message
     try:
+<<<<<<< HEAD
         # Generate response using Google Generative AI
+=======
+>>>>>>> 694168a (render deployment workflow)
         response = model.generate_content(user_message)
         bot_reply = "\n" + response.text + "\n"
     except Exception as e:
